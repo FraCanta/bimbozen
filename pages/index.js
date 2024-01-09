e2e9159796e2e944a254c84dc02195f57d286f3b
FraCanta <you@example.comfcantale14@gmail.com>
2024-01-09 14:11:24 +0100

    sito in costruzione

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,33 @@
 import Image from "next/image";
 import Logo from "@/public/assets/logo.png";
 import Donna from "@/public/assets/donna.png";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <div className="flex flex-col items-center justify-center gap-8 lg:gap-14 py-6">
-        <Image
-          src={Logo}
-          className="w-[200px] lg:w-[400px] "
-          alt="costruzione"
-        />
-        <h1 className="text-center text-3xl lg:text-[3.5rem] font-bold uppercase tracking-tight text-[#bcbcbc]">
-          Sito in costruzione
-        </h1>
+    <>
+      <Head>
+        <title>Bimbo Zen</title>
+      </Head>
+      <div className="h-screen w-full grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="flex flex-col items-center justify-center gap-8 lg:gap-14 py-6">
+          <Image
+            src={Logo}
+            className="w-[200px] lg:w-[400px] "
+            alt="costruzione"
+          />
+          <h1 className="text-center text-3xl lg:text-[3.5rem] font-bold uppercase tracking-tight text-[#bcbcbc]">
+            Sito in costruzione
+          </h1>
+        </div>
+        <div className="relative">
+          <Image
+            src={Donna}
+            className="w-full h-full lg:h-screen object-contain lg:p-4"
+            alt="costruzione"
+          />
+        </div>
       </div>
-      <div className="relative">
-        <Image
-          src={Donna}
-          className="w-full h-full lg:h-screen object-contain lg:p-4"
-          alt="costruzione"
-        />
-      </div>
-    </div>
+    </>
   );
 }
